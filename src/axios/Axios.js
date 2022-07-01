9f93ecc91bd7b7bd42c6c4ef0ee6a30fde53aef9
Gosselke Loic <gosselkeloic@gmail.com>
2022-07-01 09:46:52 +0200

    comment code fix

--- src/axios/Axios.js
+++ src/axios/Axios.js
@@ -2,8 +2,16 @@ import React from "react";
 import axios from "axios";
 import { BASE_URL } from "./Const_URL";
 
+/**
+ * static class to avoid the intance of the class
+ */
 class Requests extends React.Component {
     
+    /**
+     * fetch data for animes
+     * @param {string} params 
+     * @returns return an array with the animes payload or throw an error
+     */
     static async getAnimes(params) {
         try {
             console.log("[Axios.js][getAnimes] request was sended")
@@ -18,6 +26,10 @@ class Requests extends React.Component {
         }
     };
 
+    /**
+     * fetch recomendations animes
+     * @returns return an array with the recomendations animes payload or throw an error
+     */
     static async getRecomendations() {
         try {
             console.log("[Axios.js][getRecomendations] request was sended")
--- src/axios/Const_URL.js
+++ src/axios/Const_URL.js
@@ -1,3 +1,4 @@
+//Const with the base url for fetching data
 const BASE_URL = {
     animes: "https://api.jikan.moe/v4/anime?q=",
     recomendations: "https://api.jikan.moe/v4/recommendations/anime",
